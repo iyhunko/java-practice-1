9cb85c6d05ec70f362772807d1dcdff8eb8f3844
ivan.hunko <iyhunko@gmail.com>
2020-11-27 10:40:29 +0200

    Implement Task 4

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -1,9 +1,47 @@
 package com.epam.rd.java.basic.practice1;
 
+import java.util.regex.Pattern;
+
 public class Part4 {
 
+    private static final Pattern isNumericPattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
     public static void main(String[] args) {
-        
+        double greatestCommonDivisor;
+        double number1 = 0;
+        double number2 = 0;
+
+        for (String arg : args) {
+            if (isNumericPattern.matcher(arg).matches() && Double.parseDouble(arg) > 0) {
+                if (number1 == 0) {
+                    number1 = Double.parseDouble(arg);
+                } else {
+                    number2 = Double.parseDouble(arg);
+
+                    break;
+                }
+            }
+        }
+
+        greatestCommonDivisor = findGreatestCommonDivisor(number1, number2);
+
+        // If sum has no decimals, then print it without decimals
+        if (greatestCommonDivisor % 1 == 0) {
+            System.out.print((int) greatestCommonDivisor);
+        } else {
+            System.out.print(greatestCommonDivisor);
+        }
     }
 
+    public static double findGreatestCommonDivisor(double a, double b) {
+        while (b != 0) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+
+        return a;
+    }
 }
