02808e0bafabae4fc00f53b1336da0409f2031fe
ivan.hunko <iyhunko@gmail.com>
2020-11-27 10:25:57 +0200

    Implement Task 3

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,7 +3,20 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
-        
+        StringBuilder result = new StringBuilder();
+
+        for(String arg: args) {
+            if (!arg.trim().equals("")) {
+                // Don't add a space before new element if it's the first argument
+                if (result.length() != 0) {
+                    result.append(" ");
+                }
+
+                result.append(arg.trim());
+            }
+        }
+
+        System.out.print(result.toString().trim());
     }
 	
 }
