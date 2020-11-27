ed561d92b89bfd68ee158e2cad09fc0693b99ac7
ivan.hunko <iyhunko@gmail.com>
2020-11-27 11:08:32 +0200

    Implement Task 5

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -1,9 +1,31 @@
 package com.epam.rd.java.basic.practice1;
 
+import java.util.regex.Pattern;
+
 public class Part5 {
 
+    private static final Pattern isNumericPattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
     public static void main(String[] args) {
-        
+        double sum = 0;
+        double[] numbers = new double[args.length];
+
+        for(int i=0; i < args.length; i++){
+            if (isNumericPattern.matcher(args[i]).matches() && Double.parseDouble(args[i]) > 0) {
+                numbers[i] = Double.parseDouble(args[i]);
+            }
+        }
+
+        for (double number : numbers) {
+            sum += number;
+        }
+
+        // If sum has no decimals, then print it without decimals
+        if (sum % 1 == 0) {
+            System.out.print((int) sum);
+        } else {
+            System.out.print(sum);
+        }
     }
 	
 }
