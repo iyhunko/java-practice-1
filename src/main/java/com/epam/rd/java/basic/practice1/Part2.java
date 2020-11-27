190a0ce60105a5c747e32b5d99705fb2a64c181d
ivan.hunko <iyhunko@gmail.com>
2020-11-27 10:09:18 +0200

    Implement Task 2

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -1,9 +1,26 @@
 package com.epam.rd.java.basic.practice1;
 
+import java.util.regex.Pattern;
+
 public class Part2 {
 
-    public static void main(String[] args) {
+    private static final Pattern isNumericPattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
+    public static void main(String[] args) {
+        double sum = 0;
+
+        for(String arg: args) {
+            if (isNumericPattern.matcher(arg).matches()) {
+                sum += Double.parseDouble(arg);
+            }
+        }
+
+        // If sum has no decimals, then print it without decimals
+        if (sum % 1 == 0) {
+            System.out.print((int) sum);
+        } else {
+            System.out.print(sum);
+        }
     }
 	
 }
