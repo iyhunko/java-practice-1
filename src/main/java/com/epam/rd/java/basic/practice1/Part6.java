9b92113b600733ad52d09eba35aebde9125df445
iyhunko <iyhunko@gmail.com>
2020-11-30 16:01:48 +0200

    Remove java.util.Arrays import

--- src/main/java/com/epam/rd/java/basic/practice1/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part6.java
@@ -1,25 +1,30 @@
 package com.epam.rd.java.basic.practice1;
 
 
-import java.util.Arrays;
-
 public class Part6 {
 
     public static void main(String[] args) {
         int numberOfElements = Integer.parseInt(args[0]);
 
-        int[] result = new int[numberOfElements];
+        int[] digits = new int[numberOfElements];
 
         for (int i = 0; i < numberOfElements; i++) {
             if (i < 2) {
-                result[i] = 2 + i;
+                digits[i] = 2 + i;
             } else {
-                result[i] = result[i - 1] + i;
+                digits[i] = digits[i - 1] + i;
             }
         }
 
-        String delimiter = " ";
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < digits.length; i++) {
+            if (i != 0) {
+                result.append(" ");
+            }
+            result.append(digits[i]);
+        }
+        result.append("]");
 
-        System.out.print(Arrays.toString(result));
+        System.out.print(result.toString());
     }
 }
