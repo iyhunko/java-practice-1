a83a5d95d8d56f06e75f02728abed1ad1a332c8e
iyhunko <iyhunko@gmail.com>
2020-11-30 19:34:27 +0200

    Fix Part5

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -4,9 +4,10 @@ public class Part5 {
 
     public static void main(String[] args) {
         int sum = 0;
+        char[] digits = args[0].toCharArray();
 
-        for (String arg : args) {
-            sum += Integer.parseInt(arg);
+        for (char digit : digits) {
+            sum += Integer.parseInt(String.valueOf(digit));
         }
 
         System.out.print(sum);
